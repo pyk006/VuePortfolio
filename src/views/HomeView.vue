fe6b3d6a96da05bc211d0cbbc2255b6b21149557
pyk006 <phillipkim777@gmail.com>
2023-06-22 21:32:49 -0700

    Added in animated text effect with SCSS

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="home">
-  
-      <LandingPage msg="Welcome to Your Vue.js App"/>
-      <span class="landing-highlight a"> &#160; Phillip Kim</span>
-            <span class="landing-highlight b" >Full Stack Developer</span>
+      <span class="typed-text">{{ typeValue }}</span>
+
+      <LandingPage/>
+
+
     </div>
   </template>
   
@@ -15,36 +16,71 @@
     name: 'HomeView',
     components: {
       LandingPage
-    }
+    },
+    data: () => {
+    return {
+      typeValue: "",
+      typeStatus: false,
+      displayTextArray: ["Phillip Kim", "Full Stack", "UI/UX"],
+      typingSpeed: 100,
+      erasingSpeed: 100,
+      newTextDelay: 2000,
+      displayTextArrayIndex: 0,
+      charIndex: 0,
+    };
+  },
+  methods: {
+    typeText() {
+      if (this.charIndex < this.displayTextArray[this.displayTextArrayIndex].length) {
+        if (!this.typeStatus) this.typeStatus = true;
+        this.typeValue += this.displayTextArray[this.displayTextArrayIndex].charAt(
+          this.charIndex
+        );
+        this.charIndex += 1;
+        setTimeout(this.typeText, this.typingSpeed);
+      } else {
+        this.typeStatus = false;
+        setTimeout(this.eraseText, this.newTextDelay);
+      }
+    },
+    eraseText() {
+      if (this.charIndex > 0) {
+        if (!this.typeStatus) this.typeStatus = true;
+        this.typeValue = this.displayTextArray[this.displayTextArrayIndex].substring(
+          0,
+          this.charIndex - 1
+        );
+        this.charIndex -= 1;
+        setTimeout(this.eraseText, this.erasingSpeed);
+      } else {
+        this.typeStatus = false;
+        this.displayTextArrayIndex += 1;
+        if (this.displayTextArrayIndex >= this.displayTextArray.length)
+          this.displayTextArrayIndex = 0;
+        setTimeout(this.typeText, this.typingSpeed + 1000);
+      }
+    },
+  },
+  created() {
+   setTimeout(this.typeText, this.newTextDelay + 200);
+},
   }
   </script>
   
-  <style scoped>
+  <style lang = "scss" scoped>
   
-  .landing-highlight {
-    position: absolute;
-    font-family: 'Arvo', serif;
-    font-size: clamp(55px, 20vw, 90px);
-    font-weight: 250;
-    line-height: 1.3;
-    z-index: 9999;
-  }
-  .landing-highlight.a {
-    -webkit-transform: translate(-40px, -710px);
-            transform: translate(-40px, -710px);
-            right: 700px;
-  }
-  
-  .landing-highlight.b {
-    -webkit-transform: translate(-20px, -710px);
-            transform: translate(-20px, -710px);
-            left: 600px;
-            bottom: -450px;
-  }
-  
-  .landing-highlight.b,
-  .landing-highlight.a {
-    color: #c5efff;
-  }
+
+.typed-text {
+  position: absolute;
+  top: 55%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.typed-text {
+  color: #c3e1ff;
+  font-size: 10rem;
+}
+
   
   </style>
