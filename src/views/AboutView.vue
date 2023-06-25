9fa4fc8f3095ca842542e52174523fb6beec9446
pyk006 <phillipkim777@gmail.com>
2023-06-24 21:09:39 -0700

    Added in img hover link

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="about">
       <h1 class = "title">About Me</h1>
+      <a href="https://linkedin.com/in/phillipy-kim" target="_blank">
       <img class = "title-img" src="/Images/profile.jpg" alt="self-photo">
+    </a>
       <p class = "about-summary" style="color: rgb(206, 206, 105)">Hey there! I'm Phillip, and I've always been captivated by the thrill of problem-solving and the excitement of learning new skills. During my university days, I focused on Human Computer Interaction, driven by a desire to uncover the best solutions for user-centered design. However, along the way, I stumbled upon the world of software development.
 
 As I dived into the realms of HTML, CSS, and JavaScript, a passion ignited within me. I can vividly recall the moments when I created my very first portfolio website, crafted entirely from scratch. Sure, it may not have been the most visually stunning design, but that feeling of accomplishment was unbeatable.
@@ -19,7 +21,7 @@ As of now, I have successfully completed an incredible Full Stack Development Bo
     flex-direction: column;
     align-items: center;
     text-align: center;
-    justify-content: center;
+    justify-content: flex-start;
   }
   .title {
     color: #FF0;
@@ -28,7 +30,12 @@ As of now, I have successfully completed an incredible Full Stack Development Bo
   .title-img {
     border-radius: 50%;
     height: 250px;
+    transition: filter 0.3s ease;
   }
+  .title-img:hover {
+    filter: grayscale(100%) blur(0.5px);
+  }
+
   .about-summary {
     padding: 30px 60px;
 word-wrap: break-word;  }
